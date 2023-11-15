d17c4f65f49b74e3cccf17bf9dc68b30daa97b1b
Pibo Shongwe <piboman@yahoo.com>
2023-11-15 10:06:28 -0800

    u

--- mydiv.c
+++ mydiv.c
@@ -18,10 +18,10 @@ void myDiv(stack_t **start, unsigned int iterator)
 	if ((*start)->n == 0)
 	{
 		fprintf(stderr, "L%d: division by zero\n", iterator);
-                fclose(mont.myfile);
-                free(mont.subjects);
-                free_allstacks(*start);
-                exit(EXIT_FAILURE);
+		fclose(mont.myfile);
+		free(mont.subjects);
+		free_allstacks(*start);
+		exit(EXIT_FAILURE);
 	}
 
 	(*start)->next->n /= (*start)->n;
