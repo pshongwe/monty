80b20145376c4f3aba918152b24a0bf5483ee91f
Pibo Shongwe <piboman@yahoo.com>
2023-11-16 03:12:34 +0200

    Update data_structures.c

--- data_structures.c
+++ data_structures.c
@@ -1,17 +1,6 @@
 #include "monty.h"
 
 /**
- * myStack - sets the format of the data to a stack (LIFO)
- * @start: A pointer to the head of the stack
- * @iterator: The line number where the opcode appears
- */
-void myStack(stack_t **start, unsigned int iterator)
-{
-(void)iterator;
-(void)start;
-}
-
-/**
  * myQueue - sets the format of the data to a queue (FIFO)
  * @start: A pointer to the head of the stack
  * @iterator: The line number where the opcode appears
@@ -21,7 +10,7 @@ void myQueue(stack_t **start, unsigned int iterator)
 stack_t *current = *start;
 stack_t *newStart = NULL;
 
-(void)iterator;  // Unused parameter warning
+(void)iterator;
 
 if (current == NULL || current->next == NULL)
 return;
@@ -40,3 +29,31 @@ current = next;
 *start = newStart;
 }
 
+/**
+ * myStack - sets the format of the data to a stack (LIFO)
+ * @start: A pointer to the head of the stack
+ * @iterator: The line number where the opcode appears
+ */
+void myStack(stack_t **start, unsigned int iterator)
+{
+stack_t *current = *start;
+stack_t *newStart = NULL;
+
+(void)iterator;
+
+if (current == NULL || current->next == NULL)
+return;
+
+while (current != NULL)
+{
+stack_t *next = current->next;
+current->next = newStart;
+current->prev = NULL;
+if (newStart != NULL)
+newStart->prev = current;
+newStart = current;
+current = next;
+}
+
+*start = newStart;
+}
