5f2a9d9780312a03147d4b2d9e01f812804b50cf
Pibo Shongwe <piboman@yahoo.com>
2023-11-16 03:23:25 +0200

    Update data_structures.c

--- data_structures.c
+++ data_structures.c
@@ -7,20 +7,22 @@
  */
 void myQueue(stack_t **start, unsigned int iterator)
 {
-stack_t *current = *start;
+stack_t *next, *current = *start;
 stack_t *newStart = NULL;
 
 (void)iterator;
-
+mont.flgs = 1;
 if (current == NULL || current->next == NULL)
 return;
-
+newStart = = malloc(sizeof(stack_t));
+if (newStart == NULL)
+printf("Error\n");
 while (current->next != NULL)
 current = current->next;
 
 while (current != NULL)
 {
-stack_t *next = current->prev;
+next = current->prev;
 current->prev = newStart;
 current->next = next;
 newStart = current;
