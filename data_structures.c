0ee99161231b72bdfa4c2434793c14ac67d16f5f
Pibo Shongwe <piboman@yahoo.com>
2023-11-16 03:14:04 +0200

    Update data_structures.c

--- data_structures.c
+++ data_structures.c
@@ -8,25 +8,22 @@
 void myQueue(stack_t **start, unsigned int iterator)
 {
 stack_t *current = *start;
-stack_t *newStart = NULL;
+stack_t *last = NULL;
 
 (void)iterator;
 
 if (current == NULL || current->next == NULL)
 return;
 
-while (current != NULL)
-{
-stack_t *next = current->next;
-current->next = newStart;
-current->prev = NULL;
-if (newStart != NULL)
-newStart->prev = current;
-newStart = current;
-current = next;
-}
+while (current->next != NULL)
+current = current->next;
 
-*start = newStart;
+last = current;
+last->prev->next = NULL;
+last->prev = NULL;
+last->next = *start;
+(*start)->prev = last;
+*start = last;
 }
 
 /**
